77c0d12232b07ca008a7f976a3a87c2fbdd590db
ivysone <ivysoneshin@gmail.com>
2025-02-09 04:42:12 +0000

    commit

--- script.js
+++ script.js
@@ -70,16 +70,3 @@ const prompts = [
 ];
 
 let promptIndex = 0;
-
-function handleNo() {
-    const btnNo = document.querySelector('.no-button');
-    const btnYes = document.querySelector('.yes-button');
-    btnNo.textContent = prompts[promptIndex];
-    promptIndex = (promptIndex + 1) % prompts.length;
-    const currentSize = parseFloat(window.getComputedStyle(btnYes).fontSize);
-    btnYes.style.fontSize = `${currentSize * 1.5}px`;
-}
-
-function handleYes() {
-    window.location.href = "yes_page.html";
-}
